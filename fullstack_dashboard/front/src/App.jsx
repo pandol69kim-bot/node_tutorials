1d6afdaf5345a325c60f7d07a313b6e4e4f41336
pandol69 <pandol69.kim@gmail.com>
2026-01-26 17:27:49 +0900

    node fullstact

---
+++ fullstack_dashboard/front/src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+
+const App = () => {
+
+ const [data, setData] = useState([]);
+  
+ 
+    const fetchData = async () => {
+      const response = await axios.get("http://localhost:8000/users");
+      //console.log(response);
+      setData(response.data);
+    };
+
+    useEffect(() => {
+      fetchData();
+    }, []);
+
+  return (
+    <>
+      <h2>Data From Server</h2>
+      <div>
+        {
+          data.map((datum, idx) => (
+            <div key={idx}>
+              <h2>{datum.loyal_customer}</h2>
+              <p>{datum.month}</p>
+              <p>{datum.new_customer}</p>
+              <p>{datum.create_date}</p>
+              <p>{datum.update_date}</p>
+            </div>
+          ))
+        }
+      </div>
+    </>
+ )
+}
+
+export default App
